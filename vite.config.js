2c323e983cb51d18bcdcd04f5dc488b35dd09e7f
kefale-Dev24 <kefaleseifu@gmail.com>
2025-03-09 16:16:48 +0300

    vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  plugins: [analyze()],
   base: "/Amazon-Frontend-Clone-2025/",
   build: {
     rollupOptions: {
